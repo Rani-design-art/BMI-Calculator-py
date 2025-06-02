59b0ab2a82b3dbe345ea11136d41653fbab308e6
Rani-design-art <maharanipuansatira@gmail.com>
2025-06-02 21:40:33 +0700

    Add files via upload

--- Project5_BMICalculator.py
+++ Project5_BMICalculator.py
@@ -15,12 +15,15 @@ heightm = heightcm/100
 formula = round((weight/(heightm**2)),2)
 
 if formula < 18.5:
+    print('')
     print(f"Your BMI is {formula}\
           You are classified as {units[1]}")
 elif formula >= 18.5 and formula <= 24.9:
+    print('')
     print(f"Your BMI is {formula}\
           You are classified as {units[2]}")
 elif formula >= 25 and formula <= 29.9:
+    print('')
     print(f"Your BMI is {formula}\
           You are classified as {units[3]}")
 else:
